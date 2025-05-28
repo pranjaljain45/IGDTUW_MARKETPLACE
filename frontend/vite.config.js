9cdbb6aba77964ecd1d1be33de7a23d51d3d7d1b
Pranjaljain2007 <pranjaljain0120@gmail.com>
2025-05-29 02:30:23 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,48 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: '../backend/client',
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
-        ws: true,
-      }
-    }
+    emptyOutDir: true,
   },
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
-    }
+    },
   },
-  define: {
-    global: 'globalThis',
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis'
-      }
-    }
-  }
-});
-
-
-
-
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   envDir: '..',
-// })
+})
